10a0899662b55d97ca7e0e4df8de2c32f29bec19
“mrsjessie” <mrs.jlcone@yahoo.com>
2020-07-14 19:36:52 -0400

    <On Your Own Practice>

---
+++ project.py
@@ -0,0 +1,98 @@
+# Night Sky for July 2020
+import webbrowser
+
+print('To see the stars in July at 9PM every night over the Jacksonville, FL Sky; Pick a day in July between 1-31.')
+number = int(input())
+if number == 1:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-02#ra|8.148389767511858|dec|51.175970302576346|fov|50://www.stelvision.com/en/sky-map/";
+    webbrowser.open(url)     
+elif number == 2:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-03#ra|7.40482261808856|dec|54.98190346060039|fov|50";
+    webbrowser.open(url)
+elif number == 3:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-04#ra|6.887580075758331|dec|22.87192063476798|fov|50";
+    webbrowser.open(url)   
+elif number == 4:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-05#ra|6.956193112525941|dec|22.78137247055001|fov|50";
+    webbrowser.open(url)     
+elif number == 5:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-06#ra|7.024797172485417|dec|22.684148322665006|fov|50";
+    webbrowser.open(url)     
+elif number == 6:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-07#ra|7.093124057242104|dec|22.580265502058|fov|50";
+    webbrowser.open(url)     
+elif number == 7:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-08#ra|7.161459316230923|dec|22.47000122401696|fov|50";
+    webbrowser.open(url)   
+elif number == 8:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-09#ra|7.229785598410521|dec|22.353338097362016|fov|50";
+    webbrowser.open(url)   
+elif number == 9:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-10#ra|7.297835688281619|dec|22.23001694070899|fov|50";
+    webbrowser.open(url)   
+elif number == 10:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-11#ra|7.3658851354432056|dec|22.100591461676988|fov|50";
+    webbrowser.open(url)   
+elif number == 11:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-12#ra|7.433657447543273|dec|21.964767776753973|fov|50";
+    webbrowser.open(url)   
+elif number == 12:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-13#ra|7.501429116906209|dec|21.822564179777988|fov|50";
+    webbrowser.open(url)   
+elif number == 13:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-14#ra|7.568922668351417|dec|21.674246903303008|fov|50";
+    webbrowser.open(url)   
+elif number == 14:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-15#ra|7.636424594017522|dec|21.519549414726946|fov|50";
+    webbrowser.open(url)   
+elif number == 15:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-16#ra|7.703918525760127|dec|21.359015381703983|fov|50";
+    webbrowser.open(url)   
+elif number == 16:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-17#ra|7.771127248237177|dec|21.19210177930501|fov|50";
+    webbrowser.open(url)   
+elif number == 17:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-18#ra|7.838066186886213|dec|21.019629750401982|fov|50";
+    webbrowser.open(url)   
+elif number == 18:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-19#ra|7.905012556979385|dec|20.840768795005033|fov|50";
+    webbrowser.open(url)   
+elif number == 19:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-20#ra|7.971950933154466|dec|20.65636740689298|fov|50";
+    webbrowser.open(url)   
+elif number == 20:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-21#ra|8.03860410009474|dec|20.46612242567397|fov|50";
+    webbrowser.open(url)   
+elif number == 21:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-22#ra|8.104988466066253|dec|20.270053087956978|fov|50";
+    webbrowser.open(url)   
+elif number == 22:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-23#ra|8.17137222945917|dec|20.068425626297007|fov|50";
+    webbrowser.open(url)   
+elif number == 23:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-24#ra|8.237469840809815|dec|19.86097252519998|fov|50";
+    webbrowser.open(url)   
+elif number == 24:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-25#ra|8.303298651247637|dec|19.64824745216598|fov|50";
+    webbrowser.open(url)   
+elif number == 25:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-26#ra|8.369125876169804|dec|19.429965540637955|fov|50";
+    webbrowser.open(url)   
+elif number == 26:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-27#ra|8.434675966055472|dec|19.206412259779007|fov|50";
+    webbrowser.open(url)   
+elif number == 27:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-28#ra|8.500225413205527|dec|18.977589275322984|fov|50";
+    webbrowser.open(url)   
+elif number == 28:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-29#ra|8.56549870823066|dec|18.743755688416975|fov|50";
+    webbrowser.open(url)   
+elif number == 29:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-30#ra|8.630762343545536|dec|18.50437484245799|fov|50";
+    webbrowser.open(url)   
+elif number == 30:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-07-31#ra|8.695480685556687|dec|18.260285954346973|fov|50";
+    webbrowser.open(url)   
+elif number == 31:
+    url = "https://theskylive.com/planetarium?objects=sun-moon-mercury-venus-mars-jupiter-saturn-uranus-neptune-pluto&localdata=30.33218%7C-81.65565%7CJacksonville+FL+(US)%7CAmerica%2FNew_York%7C0&obj=sun&h=01&m=00&date=2020-08-01#ra|8.760474917346109|dec|18.011186806460977|fov|50";
+    webbrowser.open(url) 
